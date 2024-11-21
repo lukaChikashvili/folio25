82045f231dd985e75672d1b4def1fd42b0ea4552
luka chikashvili <lukachikashvili2020@gmail.com>
2024-11-21 17:29:05 +0400

    make header responsive

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -5,17 +5,21 @@ import Home from "./components/Home"
 import Projects from "./components/Projects"
 import Skills from "./components/Skills"
 
+
 function App() {
 
 
   return (
     <>
+    
+
     <Header />
-     <CanvasPage />
+    <CanvasPage />
      <Home />
      <About />
      <Skills />
      <Projects />
+ 
     </>
   )
 }
--- portfolio/src/components/Header.jsx
+++ portfolio/src/components/Header.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import cup from '../assets/cup.png'
+import { MenuIcon, Moon } from 'lucide-react'
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+
   return (
     <div className='absolute w-full bg-transparent px-12 py-6 text-white flex items-center justify-between z-10 text-md'> 
        <div className='flex items-center gap-4'>
@@ -9,11 +14,24 @@ const Header = () => {
           <h1>L.C</h1>
        </div>
 
-       <nav className='flex items-center gap-6 list-none'>
+       <nav className=' items-center gap-6 list-none hidden md:flex'>
+        <span className='text-[#FABC3F] cursor-pointer'><Moon /></span>
          <li><a href = "#">About</a></li>
          <li><a href = "#">Projects</a></li>
          <li><a href = "#">Contact</a></li>
        </nav>
+
+       <div className='md:hidden flex items-center'>
+        <button onClick={toggleMenu} className='text-white'>
+           <MenuIcon />
+        </button>
+      </div>
+
+      <div className={`md:hidden absolute top-20 left-0 w-full bg-black text-white py-4 transition-all ${isMenuOpen ? 'block' : 'hidden'}`}>
+        <li className='px-6 py-2'><a href="#">About</a></li>
+        <li className='px-6 py-2'><a href="#">Projects</a></li>
+        <li className='px-6 py-2'><a href="#">Contact</a></li>
+      </div>
     </div>
   )
 }
