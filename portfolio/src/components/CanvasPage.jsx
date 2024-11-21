7215d17237aad2317780c15962a19b3c326338aa
luka chikashvili <lukachikashvili2020@gmail.com>
2024-11-21 17:43:24 +0400

    add hero section responsivness

--- portfolio/src/components/CanvasPage.jsx
+++ portfolio/src/components/CanvasPage.jsx
@@ -1,13 +1,37 @@
 import { Canvas } from '@react-three/fiber'
 import Hero from './Hero'
 import Lights from './Lights'
+import { useEffect, useState } from 'react';
 
 const CanvasPage = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+   
+    
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768); 
+      
+    };
+
+    
+    
+    window.addEventListener('resize', handleResize);
+
+    
+    handleResize();
+
+
+    
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
      <>
        <Canvas className='canvas' camera={ { fov: 75, near: 0.2, far: 1000, position: [3, 1.5, 3.5] } }>
         <Lights />
-         <Hero />
+     
+         {!isMobile &&<Hero />}
        </Canvas>
      </>
   )
--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -5,10 +5,10 @@ const Home = () => {
     <div className='text-white w-full h-screen bg-transparent absolute top-0 left-0 p-[4rem]'>
        <div className='pt-[6rem] flex flex-col gap-4' >
           <h2 className='text-3xl'><span className='text-[#FABC3F]'>Hi, I am</span></h2>
-          <h1 className='text-[4rem] '>Luka Chikashvili</h1>
-          <p className='text-4xl'><span className='text-[#FABC3F]'>Full stack</span> web developer</p>
+          <h1 className='text-[3rem] md:text-[4rem] lg:text-[4rem] '>Luka Chikashvili</h1>
+          <p className='text-3xl md:text-3xl lg:text-4xl'><span className='text-[#FABC3F]'>Full stack</span> web developer</p>
           <span className='w-2/5 h-[0.5px] mt-2 bg-gray-400'></span>
-          <p className='text-lg mt-4 w-[38rem] text-gray-300 leading-[2.3rem]'>
+          <p className='text-lg mt-4 md:w-[38rem]  text-gray-300 leading-[2.3rem]'>
             Passionate about building interactive web applications with modern technologies like <span className='text-[#FABC3F]'>React, node</span> and <span className='text-[#FABC3F]'>three.js</span>. Bringing ideas to life with clean, efficient code.
           </p>
 
