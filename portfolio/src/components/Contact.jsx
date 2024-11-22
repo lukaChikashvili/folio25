9a5c9c5577c7188ab9efad37a6feee3d776b2b8d
luka chikashvili <lukachikashvili2020@gmail.com>
2024-11-22 23:04:23 +0400

    add social responsivness

--- portfolio/src/components/Contact.jsx
+++ portfolio/src/components/Contact.jsx
@@ -1,20 +1,27 @@
-import React from 'react'
-import { motion } from 'framer-motion'
-import { Facebook, Github, Linkedin, Mail } from 'lucide-react'
+import React from 'react';
+import { Linkedin, Github, Facebook } from 'lucide-react';
 
 const Contact = () => {
-
-     const showLink = (link) => window.open(link, '_blank' );
+  const showLink = (link) => window.open(link, '_blank');
 
   return (
-<div className='fixed bottom-4 right-8 bg-[#FABC3F] rounded-full shadow-lg shadow-black'>
-  <div className='flex flex-col gap-2 p-2 rounded-md'>
-     <Linkedin className='duration-300 ease-in cursor-pointer text-gray-600 hover:text-black hover:-translate-x-[1.5px] ' onClick={() => showLink('https://www.linkedin.com/in/luka-chikashvili/')} />
-     <Github className='duration-300 ease-in cursor-pointer text-gray-600 hover:text-black hover:-translate-x-[1.5px] ' onClick={() => showLink('https://github.com/lukaChikashvili')} />
-     <Facebook className='duration-300 ease-in cursor-pointer text-gray-600 hover:text-black hover:-translate-x-[1.5px] ' onClick={() => showLink('https://www.facebook.com/profile.php?id=61565951550341')}/>
-  </div> 
-</div>
-  )
-}
+    <div className='fixed bottom-4 right-4 md:bottom-12 md:right-8 lg:bottom-6 lg:right-8 z-10 bg-[#FABC3F] rounded-full shadow-lg shadow-black'>
+      <div className='flex flex-row md:flex-col gap-2 p-2 rounded-md'>
+        <Linkedin
+          className='duration-300 ease-in cursor-pointer text-gray-600 hover:text-black hover:-translate-x-[1.5px]'
+          onClick={() => showLink('https://www.linkedin.com/in/luka-chikashvili/')}
+        />
+        <Github
+          className='duration-300 ease-in cursor-pointer text-gray-600 hover:text-black hover:-translate-x-[1.5px]'
+          onClick={() => showLink('https://github.com/lukaChikashvili')}
+        />
+        <Facebook
+          className='duration-300 ease-in cursor-pointer text-gray-600 hover:text-black hover:-translate-x-[1.5px]'
+          onClick={() => showLink('https://www.facebook.com/profile.php?id=61565951550341')}
+        />
+      </div>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
